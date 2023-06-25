a8acdd12b7118bf4744144b54eab74bec9c8d2c5
Daniele <daniele.maggiore.1690@gmail.com>
2023-06-25 13:52:15 +0200

    Init

--- src/index.js
+++ src/index.js
@@ -1,17 +1,109 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import './styles/index.scss';
+import SmoothScroll from './components/SmoothScroll';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <>
+    <SmoothScroll />
+    <h1>ciao</h1>
+    <h1>ciao</h1>
+    <h1>ciao</h1>
+    <h1>ciao</h1>
+    <h1>ciao</h1>
+    <h1>ciao</h1>
+    <h1>ciao</h1>
+    <h1>ciao</h1>
+    <h1>ciao</h1>
+    <h1>ciao</h1>
+    <h1>ciao</h1>
+    <h1>ciao</h1>
+    <h1>ciao</h1>
+    <h1>ciao</h1>
+    <h1>ciao</h1>
+    <h1>ciao</h1>
+    <h1>ciao</h1>
+    <h1>ciao</h1>
+    <h1>ciao</h1>
+    <h1>ciao</h1>
+    <h1>ciao</h1>
+    <h1>ciao</h1>
+    <h1>ciao</h1>
+    <h1>ciao</h1>
+    <h1>ciao</h1>
+    <h1>ciao</h1>
+    <h1>ciao</h1>
+    <h1>ciao</h1>
+    <h1>ciao</h1>
+    <h1>ciao</h1>
+    <h1>ciao</h1>
+    <h1>ciao</h1>
+    <h1>ciao</h1>
+    <h1>ciao</h1>
+    <h1>ciao</h1>
+    <h1>ciao</h1>
+    <h1>ciao</h1>
+    <h1>ciao</h1>
+    <h1>ciao</h1>
+    <h1>ciao</h1>
+    <h1>ciao</h1>
+    <h1>ciao</h1>
+    <h1>ciao</h1>
+    <h1>ciao</h1>
+    <h1>ciao</h1>
+    <h1>ciao</h1>
+    <h1>ciao</h1>
+    <h1>ciao</h1>
+    <h1>ciao</h1>
+    <h1>ciao</h1>
+    <h1>ciao</h1>
+    <h1>ciao</h1>
+    <h1>ciao</h1>
+    <h1>ciao</h1>
+    <h1>ciao</h1>
+    <h1>ciao</h1>
+    <h1>ciao</h1>
+    <h1>ciao</h1>
+    <h1>ciao</h1>
+    <h1>ciao</h1>
+    <h1>ciao</h1>
+    <h1>ciao</h1>
+    <h1>ciao</h1>
+    <h1>ciao</h1>
+    <h1>ciao</h1>
+    <h1>ciao</h1>
+    <h1>ciao</h1>
+    <h1>ciao</h1>
+    <h1>ciao</h1>
+    <h1>ciao</h1>
+    <h1>ciao</h1>
+    <h1>ciao</h1>
+    <h1>ciao</h1>
+    <h1>ciao</h1>
+    <h1>ciao</h1>
+    <h1>ciao</h1>
+    <h1>ciao</h1>
+    <h1>ciao</h1>
+    <h1>ciao</h1>
+    <h1>ciao</h1>
+    <h1>ciao</h1>
+    <h1>ciao</h1>
+    <h1>ciao</h1>
+    <h1>ciao</h1>
+    <h1>ciao</h1>
+    <h1>ciao</h1>
+    <h1>ciao</h1>
+    <h1>ciao</h1>
+    <h1>ciao</h1>
+    <h1>ciao</h1>
+    <h1>ciao</h1>
+    <h1>ciao</h1>
+    <h1>ciao</h1>
+    <h1>ciao</h1>
+    <h1>ciao</h1>
+    <h1>ciao</h1>
+    <h1>ciao</h1>
+    <h1>ciao</h1>
+  </>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
---
+++ src/js/firebase.js
@@ -0,0 +1,13 @@
+import { initializeApp } from "firebase/app";
+import * as config from "../../config";
+
+const firebaseConfig = {
+  apiKey: config.apiKey,
+  authDomain: config.authDomain,
+  projectId: config.projectId,
+  storageBucket: config.storageBucket,
+  messagingSenderId: config.messagingSenderId,
+  appId: config.appId,
+};
+
+initializeApp(firebaseConfig);
